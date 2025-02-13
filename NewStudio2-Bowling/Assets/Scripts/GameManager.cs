8ca4017b16cd92ab3615c48af60201259ae0a590
juliapetrie <julia.petrie@icloud.com>
2025-02-12 21:37:48 -0800

    debugging score counter

--- NewStudio2-Bowling/Assets/Scripts/GameManager.cs
+++ NewStudio2-Bowling/Assets/Scripts/GameManager.cs
@@ -1,43 +1,3 @@
-// using TMPro;
-// using UnityEngine;
-
-// public class GameManager : MonoBehaviour
-// {
-//     [SerializeField] private float score = 0;
-//     // [SerializeField] private TextMeshPro scoreText;
-//     [SerializeField] private BallController ball;
-
-//     [SerializeField] private GameObject pinCollection;
-
-//     [SerializeField] private Transform pinAnchor;
-//     [SerializeField] private InputManager inputManager;
-//     [SerializeField] private TextMeshProUGUI scoreText;
-
-//     private FallTrigger[] pins;
-//     private void Start(){
-//         //We find all objects of type FallTrigger
-
-//         inputManager.OnResetPressed.AddListener(HandleReset);
-//         SetPins();
-
-//         pins = FindObjectsByType<FallTrigger>(FindObjectsSortMode.None); 
-
-
-//         // Loop through array and add incrementScore function as their listener
-//         foreach (FallTrigger pin in pins)
-//         {
-//             pin.OnPinFall.AddListener(IncrementScore);
-//         }
-//     }
-//     private void IncrementScore()
-//     {
-//        score++; 
-//        scoreText.text = $"Score: {score}";
-
-//     }
-   
-// }
-
 using TMPro;
 using UnityEngine;
 
@@ -56,15 +16,15 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         // Adding the HandleReset function as a listener to our OnResetPressedEvent
-        inputManager.OnResetPressed.AddListener(HandleReset);
+       inputManager.OnResetPressed.AddListener(HandleReset);
         SetPins();
         
-        // Find all FallTrigger objects and add IncrementScore listener
-        fallTriggers = FindObjectsByType<FallTrigger>(FindObjectsSortMode.None);
-        foreach (FallTrigger pin in fallTriggers)
-        {
-            pin.OnPinFall.AddListener(IncrementScore);
-        }
+        // // Find all FallTrigger objects and add IncrementScore listener
+        // fallTriggers = FindObjectsByType<FallTrigger>(FindObjectsSortMode.None);
+        // foreach (FallTrigger pin in fallTriggers)
+        // {
+        //     pin.OnPinFall.AddListener(IncrementScore);
+        // }
     }
 
     private void HandleReset()
@@ -84,6 +44,13 @@ public class GameManager : MonoBehaviour
             }
             Destroy(pinObjects);
         }
+        if (fallTriggers != null)
+    {
+        foreach (FallTrigger pin in fallTriggers)
+        {
+            pin.OnPinFall.RemoveListener(IncrementScore);
+        }
+    }
 
         // Instantiate a new set of pins at the pin anchor transform
         // pinObjects = Instantiate(pinCollection, pinAnchor.transform.position, Quaternion.identity, transform);
